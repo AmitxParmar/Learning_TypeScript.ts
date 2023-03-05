ffd4368ac2aaaacaf2d130b151be4c124f8f9f36
AmitxParmar <amitparmar901@gmail.com>
2023-03-05 19:43:45 +0530

    #day13 - revision basics

---
+++ Day13/main.ts
@@ -0,0 +1,11 @@
+console.log("Hey lets go");
+
+function add(n1: number, n2: numbers) {
+    return n1 + n2;
+}
+console.log(add(3, 5));
+
+let meal: number | string;
+meal = ['wheat', 4];
+
+console.log(typeof meal);

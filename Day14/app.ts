9028ab6e8b18a268977b8f630f7a9528d0314ee0
AmitxParmar <amitparmar901@gmail.com>
2023-03-07 20:38:04 +0530

    complete: app.ts

---
+++ Day14/app.ts
@@ -0,0 +1,34 @@
+/* const person: {
+    name: string;
+    age: number
+} = {
+    name: "George",
+    age: 22
+}; */
+
+const person: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string]
+} = {
+    name: 'George',
+    age: 22,
+    hobbies: ['coding', 'jogging', 'music'],
+    role: [2, 'author'] //union type
+};
+
+person.role.push("Runner");
+person.role[0] = 40;
+
+let faActivities: string[];
+favActivities = ['sports'];
+
+// console.log(person);
+// console.log(person.age);
+
+// person.hobbies.forEach(hobby => {
+console.log(hobby.tpUpperCase());
+});
+
+console.log(person.role);

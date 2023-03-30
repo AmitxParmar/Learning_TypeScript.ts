883f92e1aa9bb5c002c8fe426d854e6911e252ce
AmitxParmar <amitparmar901@gmail.com>
2023-03-30 23:36:26 +0530

    Day18: Practice random

---
+++ Day18/1.ts
@@ -0,0 +1,15 @@
+let num: number = 123_456_787;
+//123456789;
+let tutorial: string = "TypeScript Basic";
+let is_uploaded: boolean = true;
+let level;
+
+// hover on it you can see it_show type "any" means it can be number , boolean, string
+// example
+// level = 1;
+// level = "Amit";
+// function print(data) // we can remove this under line on "data" just press ctrl+p if you have window and select tsconfig.json and
+// {
+//console.log(data)
+//}
+// bad practice because doing this whole typescript ideas is fail, So, never do this;
---
+++ Day18/2.ts
@@ -0,0 +1,5 @@
+// Array
+let numbers = [1, 2, 3, "45"]; // We make array in venilla javascript as this and also we can enter string in array this i valid code. but
+// how we declare array in TypeScript
+
+// let array:number[]=[1,2,3,"45"] //
